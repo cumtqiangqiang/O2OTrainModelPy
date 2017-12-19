51b104432b20ef89938230714a686b41a79b5d2c
baiyang <baiyang@ainemo.com>
2017-12-19 23:30:19 +0800

    feature combine

--- Test/DateDiff.py
+++ Test/DateDiff.py
@@ -19,45 +19,43 @@ if __name__ == '__main__':
     # data =  data1.append(data2,ignore_index=True)
     # data.to_csv(r'C:\Users\UC227911\Desktop\Mine\O2O\O2OTrainModelPy\Resource\features\offline\trainUserMerFeature\user_merchant_feature.csv',index = False)
 
-    # path = r'C:\Users\UC227911\Desktop\Mine\O2O\O2OTrainModelPy\Resource\features\online\trainMerFeature'
-    # result = None
-    # sum = 0
-    # for i in range(15):
-    #     if i < 10:
-    #        file_name = '/part-0000'+str(i)
-    #     else:
-    #         file_name = '/part-000'+str(i)
-    #     path1 = path + file_name
-    #     data = pd.read_csv(path1)
-    #     print(str(i) + ":" +  str(data.shape))
-    #     sum += data.iloc[:,0].size
-    #     if i == 0:
-    #         result = data
-    #     else:
-    #         result = result.append(data)
+    path = '../Resource/features/online/trainUserMerFeature'
+    result = None
+    sum = 0
+    for i in range(15):
+        if i < 10:
+           file_name = '/part-0000'+str(i)
+        else:
+            file_name = '/part-000'+str(i)
+        path1 = path + file_name
+        data = pd.read_csv(path1)
+        print(str(i) + ":" +  str(data.shape))
+        sum += data.iloc[:,0].size
+        if i == 0:
+            result = data
+        else:
+            result = result.append(data)
+
+    print(sum)
+    print(result.shape)
+
+    result.to_csv('../Resource/features/online/trainUserMerFeature/user_merchant_feature.csv',index=False)
+
+    # offline_data = pd.read_csv('../Resource/tb01ccf_offline_stage1_train.csv')
+    # online_data = pd.read_csv('../Resource/tb02ccf_online_stage1_train.csv')
     #
-    # print(sum)
-    # print(result.shape)
+    # offline_user_set =  set(offline_data['User_id'].tolist())
+    # online_user_set = set(online_data['User_id'].tolist())
+    # train_on_off_intersection =  offline_user_set & online_user_set
     #
-    # result.to_csv(
-    #     r'C:\Users\UC227911\Desktop\Mine\O2O\O2OTrainModelPy\Resource\features\online\trainMerFeature\merchant_feature.csv',
-    #     index=False)
-
-    offline_data = pd.read_csv('../Resource/tb01ccf_offline_stage1_train.csv')
-    online_data = pd.read_csv('../Resource/tb02ccf_online_stage1_train.csv')
-
-    offline_user_set =  set(offline_data['User_id'].tolist())
-    online_user_set = set(online_data['User_id'].tolist())
-    train_on_off_intersection =  offline_user_set & online_user_set
-
-    print("offline user cnt :" + str(len(offline_user_set)))
-    print("online user cnt :" + str(len(online_user_set)))
-    print("offline & online user cnt :" + str(len(train_on_off_intersection)))
-
-    test_offline_data = pd.read_csv('../Resource/tb03ccf_offline_stage1_test_revised.csv')
-    test_offline_user_set = set(test_offline_data['User_id'].tolist())
-    test_on_off_intersection =  test_offline_user_set & online_user_set
-
-    print("test data offline  :" + str(len(test_offline_user_set)))
-    print("test data offline & online user cnt :" + str(len(test_on_off_intersection)))
+    # print("offline user cnt :" + str(len(offline_user_set)))
+    # print("online user cnt :" + str(len(online_user_set)))
+    # print("offline & online user cnt :" + str(len(train_on_off_intersection)))
+    #
+    # test_offline_data = pd.read_csv('../Resource/tb03ccf_offline_stage1_test_revised.csv')
+    # test_offline_user_set = set(test_offline_data['User_id'].tolist())
+    # test_on_off_intersection =  test_offline_user_set & online_user_set
+    #
+    # print("test data offline  :" + str(len(test_offline_user_set)))
+    # print("test data offline & online user cnt :" + str(len(test_on_off_intersection)))
 
--- train.py
+++ train.py
@@ -28,9 +28,34 @@ def get_time_diff(start,end):
         return  False
 
 if __name__ == '__main__':
-    raw_offline_data =  pd.read_csv(TRAIN_OFFLINE_DATA_PATH)
-    data = add_label(raw_offline_data)
-    print(data)
+    # raw_offline_data =  pd.read_csv(TRAIN_OFFLINE_DATA_PATH)
+    # data = add_label(raw_offline_data)
+    #
+    # data.to_csv('Resource/trainLabelData/train_label_data.csv',index=False)
+    label_data = pd.read_csv('Resource/trainLabelData/train_label_data.csv')
+    offline_user_feature = pd.read_csv('Resource/features/offline/trainUserFeature/user_feature.csv')
+    offline_mer_feature = pd.read_csv('Resource/features/offline/trainMerFeature/merchant_feature.csv')
+    offline_user_mer_feature = pd.read_csv('Resource/features/offline/trainUserMerFeature/user_merchant_feature.csv')
+
+    online_user_feature = pd.read_csv('Resource/features/online/trainUserFeature/user_feature.csv')
+    online_mer_feature = pd.read_csv('Resource/features/online/trainMerFeature/merchant_feature.csv')
+    online_user_mer_feature = pd.read_csv('Resource/features/online/trainUserMerFeature/user_merchant_feature.csv')
+
+    user_feature = pd.merge(online_user_feature,on='User_id',how='outer')
+    user_feature.tocsv('Resource/features/user_features.csv')
+    merchant_feature = pd.merge(online_mer_feature,on='Merchant_id',how='outer')
+
+    merchant_feature.tocsv('Resource/features/mer_features.csv')
+
+
+
+
+
+
+
+
+
+
 
 
 
