713e6a12eb3bc30dc7936f4b5b6ebcfb6bb49171
qq <cumtqiangqiang@163.com>
2017-12-26 17:57:30 +0800

    modified model

--- svm_train_model.py
+++ svm_train_model.py
@@ -4,34 +4,69 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from  constants import  *
-
+from sklearn.metrics import  roc_auc_score
 def cal_average_auc(df):
-    print(df)
+    grouped = df.groupby('Coupon_id', as_index=False).apply(lambda x: calc_auc(x))
+    return grouped['auc'].mean(skipna=True)
 
 
+def calc_auc(df):
+    coupon = df['Coupon_id'].iloc[0]
+    y_true = df['label'].values
+    if len(np.unique(y_true)) != 2:
+        auc = np.nan
+    else:
+        y_pred = df['predict'].values
+        auc = roc_auc_score(np.array(y_true), np.array(y_pred))
+    return pd.DataFrame({'Coupon_id': [coupon], 'auc': [auc]})
 if __name__ == '__main__':
 
     label_data = pd.read_csv(less_label_data)
     data = pd.read_csv(less_feature_data).astype(float)
 
-    x_train, x_test, y_train, y_test = train_test_split(data, label_data['label'].astype(float), random_state=1, train_size=0.6)
+
+    x_train, x_test, y_train, y_test = train_test_split(data, label_data,
+                                                        random_state=1, train_size=0.8)
+
+    y_train_label = y_train['label'].astype(float)
+    y_test_label = y_test['label'].astype(float)
 
     # 分类器
-    clf = svm.SVC(C=0.1, kernel='linear', decision_function_shape='ovr')
+    clf_param = (('linear', 0.1), ('linear', 0.5), ('linear', 1), ('linear', 2),
+                 ('rbf', 1, 0.1), ('rbf', 1, 1), ('rbf', 1, 10), ('rbf', 1, 100),
+                 ('rbf', 5, 0.1), ('rbf', 5, 1), ('rbf', 5, 10), ('rbf', 5, 100))
+    # clf = svm.SVC(C=0.1, kernel='linear', decision_function_shape='ovr')
     # clf = svm.SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr')
-    clf.fit(x_train, y_train.ravel())
+    for i, param in enumerate(clf_param):
+        clf = svm.SVC(C=param[1], kernel=param[0])
+        if param[0] == 'rbf':
+            clf.gamma = param[2]
+            print('高斯核，C=%.1f，$\gamma$ =%.1f' % (param[1], param[2]))
+        else:
+            print('线性核，C=%.1f' % param[1])
+        clf.fit(x_train, y_train_label.ravel())
 
-    y_pre = clf.predict(x_test)
+        y_pre = clf.predict(x_test)
+        y_pre_df = pd.DataFrame(pd.Series(y_pre),columns=['predict'])
 
-    print(len(y_pre))
-    label_data['predict']=pd.Series(y_pre)
-    
+        y_train_pre = clf.predict(x_train)
+        y_train_df = pd.DataFrame(pd.Series(y_train_pre),columns=['predict'])
 
+        y_train_reindex = y_train.reset_index(drop=True)
+        y_test_reindex = y_test.reset_index(drop=True)
 
+        test_evalute = y_test_reindex[['Coupon_id','label']].join(y_pre_df)
 
-    # 准确率
-    print(clf.score(x_train, y_train))  # 精度
-    print('训练集准确率：', accuracy_score(y_train, clf.predict(x_train)))
-    print('--------------------------------------------------------------')
-    print(clf.score(x_test, y_test))
-    print('测试集准确率：', accuracy_score(y_test, clf.predict(x_test)))
+        train_evaluete = y_train_reindex[['Coupon_id','label']].join(y_train_df)
+
+        print(cal_average_auc(test_evalute))
+        print(cal_average_auc(train_evaluete))
+
+        # print(y_pre)
+
+        # 准确率
+        print(clf.score(x_train, y_train_label))  # 精度
+        print('训练集准确率：', accuracy_score(y_train_label, clf.predict(x_train)))
+        print(clf.score(x_test, y_test_label))
+        print('测试集准确率：', accuracy_score(y_test_label, clf.predict(x_test)))
+        print('--------------------------------------------------------------')
--- train_model.py
+++ train_model.py
@@ -5,31 +5,58 @@ from  constants import  *
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import  roc_auc_score
 # from memory_profiler import profile
-import gc
+def cal_average_auc(df):
+    grouped = df.groupby('Coupon_id', as_index=False).apply(lambda x: calc_auc(x))
+    return grouped['auc'].mean(skipna=True)
 
+
+
+def calc_auc(df):
+    coupon = df['Coupon_id'].iloc[0]
+    y_true = df['label'].values
+    if len(np.unique(y_true)) != 2:
+        auc = np.nan
+    else:
+        y_pred = df['predict'].values
+        auc = roc_auc_score(np.array(y_true), np.array(y_pred))
+    return pd.DataFrame({'Coupon_id': [coupon], 'auc': [auc]})
 if __name__ == '__main__':
 
     label_data = pd.read_csv(less_label_data)
     train_features = pd.read_csv(less_feature_data).astype(float)
 
-    y_label = label_data['label'].astype(float)
+
     # train_features['label'] = pd.Series(y_label.ravel())
 
 
-    x_train,x_test,y_train,y_test =train_test_split(train_features,y_label,
+    x_train,x_test,y_train,y_test =train_test_split(train_features,label_data,
                                                     random_state=1,train_size=0.8)
 
-
-    # print(x_train.info())
+    y_train_label = y_train['label'].astype(float)
+    y_test_label = y_test['label'].astype(float)
+    # print(y_label)
     lr = Pipeline([('sc', StandardScaler()),
                    ('poly', PolynomialFeatures(degree=3)),
                    ('clf', LogisticRegression())])
 
-    lr.fit(x_train, y_train.ravel())
+
+    lr.fit(x_train, y_train_label.ravel())
 
     y_hat = lr.predict(x_test)
-    print(print('准确度：%.2f%%' % (100*np.mean(y_hat == y_test.ravel()))))
+    y_pre_df = pd.DataFrame(pd.Series(y_hat), columns=['predict'])
+
+    y_train_reindex = y_train.reset_index(drop=True)
+    y_test_reindex = y_test.reset_index(drop=True)
+
+
+    test_evalute = y_test_reindex[['Coupon_id', 'label']].join(y_pre_df)
+    print(test_evalute[['Coupon_id','label','predict']])
+
+
+    print(cal_average_auc(test_evalute))
+    print('准确度：%.2f%%' % (100*np.mean(y_hat == y_test_label.ravel())))
 
 
 
